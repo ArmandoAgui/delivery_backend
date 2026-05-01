550dcb49920290f4847a6c51b7be82d4612fe61b
ArmandoAgui <00174323@uca.edu.sv>
2026-05-01 09:23:10 -0600

    Initial delivery backend structure

---
+++ src/main/java/sv/edu/uca/delivery/backend/DeliveryBackendApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.uca.delivery.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sv/edu/uca/delivery/backend/DeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.uca.delivery.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
