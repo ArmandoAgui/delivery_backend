c05ff32915e0078c644102c6bc25f801b6189be8
dianaerazo <00149823@uca.edu.sv>
2026-06-09 23:16:47 -0600

    feat: implement user service and controller

---
+++ src/main/java/sv/edu/uca/delivery/backend/user/controller/UserController.java
@@ -0,0 +1,23 @@
+package sv.edu.uca.delivery.backend.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import sv.edu.uca.delivery.backend.user.dto.response.UserResponse;
+import sv.edu.uca.delivery.backend.user.service.UserService;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public UserResponse getUserById(
+            @PathVariable UUID id
+    ) {
+        return userService.findById(id);
+    }
+}
